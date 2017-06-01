cfe7fb0f9fa57f8ca4c2590859e552a6783a7e5a
Ian Horsman <ianphorsman@gmail.com>
2017-05-31 23:51:41 -0400

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root '/home#index'
+
+  mount ActionCable.server => '/cable'
+
+  get '/poll/:poll_id' => 'polls#show'
+
+  get '/home/show/:poll_id' => 'home#show'
+
+  post '/poll/create' => 'polls#create'
+  post '/poll/:poll_id/vote/:poll_selection_id' => 'polls#vote'
+
 end
